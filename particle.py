52a5a9ac9e1121266c95bc1e5aaadc0d97b06081
Imran Alam <75203259+Imranalam28@users.noreply.github.com>
2023-06-15 08:37:47 +0530

    Update particle.py

--- particle.py
+++ particle.py
@@ -1,8 +1,5 @@
-import pygame
+import arcade
 import random
-
-# Initialize Pygame
-pygame.init()
 
 # Screen dimensions
 SCREEN_WIDTH = 800
@@ -27,54 +24,44 @@ class Particle:
         self.y += self.dy
         self.lifespan -= 1
 
-    def draw(self, screen):
-        pygame.draw.circle(screen, self.color, (int(self.x), int(self.y)), self.radius)
+    def draw(self):
+        arcade.draw_circle_filled(self.x, self.y, self.radius, self.color)
 
-# Create the screen
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-pygame.display.set_caption("Particle System")
+# Game class
+class MyGame(arcade.Window):
+    def __init__(self, width, height):
+        super().__init__(width, height, "Particle Example")
+        self.particles = []
 
-# Particle list
-particles = []
+    def setup(self):
+        # Create particles
+        for _ in range(100):
+            x = random.randrange(SCREEN_WIDTH)
+            y = random.randrange(SCREEN_HEIGHT)
+            dx = random.uniform(-1, 1)
+            dy = random.uniform(-1, 1)
+            radius = random.uniform(2, 5)
+            color = arcade.color.WHITE
+            lifespan = random.randint(60, 120)
+            particle = Particle(x, y, dx, dy, radius, color, lifespan)
+            self.particles.append(particle)
 
-# Game loop
-running = True
-clock = pygame.time.Clock()
+    def on_draw(self):
+        arcade.start_render()
+        for particle in self.particles:
+            particle.draw()
 
-while running:
-    # Handle events
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
+    def update(self, delta_time):
+        for particle in self.particles:
+            particle.update()
 
-    # Clear the screen
-    screen.fill((0, 0, 0))
+            if particle.lifespan <= 0:
+                self.particles.remove(particle)
 
-    # Create new particles
-    for _ in range(10):
-        x = random.uniform(0, SCREEN_WIDTH)
-        y = random.uniform(0, SCREEN_HEIGHT)
-        dx = random.uniform(-1, 1)
-        dy = random.uniform(-1, 1)
-        radius = random.uniform(2, 5)
-        color = WHITE
-        lifespan = random.randint(30, 60)
-        particle = Particle(x, y, dx, dy, radius, color, lifespan)
-        particles.append(particle)
+def main():
+    game = MyGame(SCREEN_WIDTH, SCREEN_HEIGHT)
+    game.setup()
+    arcade.run()
 
-    # Update particles
-    for particle in particles:
-        particle.update()
-        if particle.lifespan <= 0:
-            particles.remove(particle)
-
-    # Draw particles
-    for particle in particles:
-        particle.draw(screen)
-
-    # Update the display
-    pygame.display.flip()
-    clock.tick(60)
-
-# Quit the game
-pygame.quit()
+if __name__ == "__main__":
+    main()
